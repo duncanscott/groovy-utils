0d2f9ca0c04b38c4202e7b94664afa046385e446
Duncan Scott <dnscott@gmail.com>
2019-10-26 23:25:52 -0700

    version 1.6.3 EnumConverter no longer uses OnDemandCache

--- libraries/enum-util/src/main/groovy/duncanscott/org/groovy/utils/enumutil/EnumConverter.groovy
+++ libraries/enum-util/src/main/groovy/duncanscott/org/groovy/utils/enumutil/EnumConverter.groovy
@@ -1,34 +1,29 @@
 package duncanscott.org.groovy.utils.enumutil
 
-import duncanscott.org.groovy.utils.ondemandcache.OnDemandCache
-
 class EnumConverter<K extends Enum> {
 
-    private final OnDemandCache<Map<String, K>> cachedConversionMap = new OnDemandCache<>()
+    private final Map<String, K> conversionMap = new HashMap<>()
+
     private final Class<K> enumType
     final boolean caseSensitive
 
     EnumConverter(Class<K> enumType, boolean caseSensitive) {
         this.enumType = enumType
         this.caseSensitive = caseSensitive
+        populateConversionMap()
     }
 
     EnumConverter(Class<K> enumType) {
-        this.enumType = enumType
-        this.caseSensitive = true
+        this(enumType,true)
     }
 
-    private Map<String, K> getConversionMap() {
-        return cachedConversionMap.fetch {
-            Map<String, K> cMap = [:]
-            enumType.values().each { K enumValue ->
-                String strVal = caseSensitive ? enumValue.toString().trim() : enumValue.toString().trim().toLowerCase()
-                if (cMap.containsKey(strVal)) {
-                    throw new RuntimeException("duplicate ${caseSensitive ? 'case-sensitive' : 'case-insensitive'} enum value [${strVal}]")
-                }
-                cMap[strVal] = enumValue
+    private void populateConversionMap() {
+        enumType.values().each { K enumValue ->
+            String strVal = caseSensitive ? enumValue.toString().trim() : enumValue.toString().trim().toLowerCase()
+            if (conversionMap.containsKey(strVal)) {
+                throw new RuntimeException("duplicate ${caseSensitive ? 'case-sensitive' : 'case-insensitive'} enum value [${strVal}]")
             }
-            cMap
+            conversionMap[strVal] = enumValue
         }
     }
 
