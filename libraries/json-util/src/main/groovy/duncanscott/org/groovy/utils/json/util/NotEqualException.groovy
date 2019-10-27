372b4b69079b0ca59bdf42dcca7a7445444c4304
Duncan Scott <dnscott@gmail.com>
2019-10-26 21:25:29 -0700

    tweak to NotEqualException

--- libraries/json-util/src/main/groovy/duncanscott/org/groovy/utils/json/util/NotEqualException.groovy
+++ libraries/json-util/src/main/groovy/duncanscott/org/groovy/utils/json/util/NotEqualException.groovy
@@ -9,12 +9,13 @@ class NotEqualException extends RuntimeException {
     final Object o2
 
     NotEqualException(o1, o2) {
+        super('JSON elements are not equal')
         this.o1 = o1
         this.o2 = o2
     }
 
     String toString() {
-        StringBuffer b = new StringBuffer("JSON elements not equal:")
+        StringBuilder b = new StringBuilder()
         b << "\n#### Element 1 ####################################\n"
         b << JsonUtil.toString(o1)
         b << "\n#### Element 2 ####################################\n"
