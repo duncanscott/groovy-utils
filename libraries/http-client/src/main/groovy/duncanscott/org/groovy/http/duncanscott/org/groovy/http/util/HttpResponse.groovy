ce5bc7ff0a431f5f5b33c054377ba79a03107f00
Duncan Scott <dnscott@gmail.com>
2023-08-13 16:30:11 -0700

    delegate TextResponse

--- libraries/http-client/src/main/groovy/duncanscott/org/groovy/http/duncanscott/org/groovy/http/util/HttpResponse.groovy
+++ libraries/http-client/src/main/groovy/duncanscott/org/groovy/http/duncanscott/org/groovy/http/util/HttpResponse.groovy
@@ -5,44 +5,20 @@ import org.apache.hc.core5.http.HttpStatus
 
 class HttpResponse {
 
-    TextResponse textResponse
+    @Delegate TextResponse textResponse
 
     boolean getSuccess() {
-        statusCode in [HttpStatus.SC_CREATED, HttpStatus.SC_OK]
-    }
-
-    String getRequestUri() {
-        textResponse?.requestUri
-    }
-
-    String getRequestMethod() {
-        textResponse?.requestMethod
-    }
-
-    String getText() {
-        textResponse?.text
-    }
-
-    int getStatusCode() {
-        textResponse ? textResponse.statusCode : 0
-    }
-
-    String getReasonPhrase() {
-        textResponse?.reasonPhrase
-    }
-
-    Locale getLocal() {
-        textResponse?.locale
+        textResponse?.statusCode in [HttpStatus.SC_CREATED, HttpStatus.SC_OK]
     }
 
     String toString() {
         StringBuilder stringBuilder = new StringBuilder(this.class.simpleName)
         stringBuilder.append('(')
-        if (requestUri) {
-            stringBuilder.append(requestUri)
-            stringBuilder.append(':' + statusCode)
+        if (textResponse?.requestUri) {
+            stringBuilder.append(textResponse?.requestUri)
+            stringBuilder.append(':' + textResponse.statusCode)
             if (reasonPhrase) {
-                stringBuilder.append(':' + reasonPhrase)
+                stringBuilder.append(':' + textResponse.reasonPhrase)
             }
         }
         stringBuilder.append(')')
