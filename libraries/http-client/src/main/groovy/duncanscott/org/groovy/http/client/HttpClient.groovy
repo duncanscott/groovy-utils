f214bdeb390f584d073bec6f87f4b1fe91d5bec3
Duncan Scott <dnscott@gmail.com>
2025-09-28 14:16:40 -0700

    refine HttpClient

--- libraries/http-client/src/main/groovy/duncanscott/org/groovy/http/client/HttpClient.groovy
+++ libraries/http-client/src/main/groovy/duncanscott/org/groovy/http/client/HttpClient.groovy
@@ -1,28 +1,32 @@
 package duncanscott.org.groovy.http.client
 
-
 import duncanscott.org.groovy.http.util.InvalidUrlException
 import duncanscott.org.groovy.http.util.RequestHeader
 import duncanscott.org.groovy.javautil.http.InputStreamHandler
-import duncanscott.org.groovy.javautil.http.RequestProcessor
-import org.apache.hc.client5.http.entity.EntityBuilder
-import org.apache.hc.core5.http.*
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient
+import org.apache.hc.client5.http.impl.classic.HttpClients
+import org.apache.hc.core5.http.ClassicHttpRequest
+import org.apache.hc.core5.http.ContentType
+import org.apache.hc.core5.http.HttpEntity
+import org.apache.hc.core5.http.HttpHeaders
+import org.apache.hc.core5.http.io.entity.EntityUtils
+import org.apache.hc.core5.http.io.entity.InputStreamEntity
 import org.apache.hc.core5.http.io.entity.StringEntity
 import org.apache.hc.core5.http.io.support.ClassicRequestBuilder
 
 import java.lang.reflect.Constructor
 
-// https://hc.apache.org/httpcomponents-client-5.2.x/quickstart.html
+class HttpClient<K extends HttpResponse> implements Closeable {
 
-class HttpClient<K extends HttpResponse> {
-
-    private String authorizationHeader
     private final Constructor<K> responseConstructor
+    private final CloseableHttpClient internalClient
+
     final List<RequestHeader> defaultHeaders = []
+    String authorizationHeader
 
     HttpClient(Class<K> responseClass) {
         this.responseConstructor = responseClass.getConstructor()
-        this.authorizationHeader = null
+        this.internalClient = HttpClients.createDefault()
     }
 
     HttpClient setAuthorizationHeader(String username, String password) {
@@ -30,180 +34,127 @@ class HttpClient<K extends HttpResponse> {
         return this
     }
 
-    K get(String url) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.get()
-        addHeaders(builder, defaultHeaders)
-        submitRequest(builder, url)
+    @Override
+    void close() throws IOException {
+        internalClient.close()
     }
 
-    /**
-     *
-     * @param url
-     * @param inputStreamHandler
-     * @param headers
-     * @return
-     *
-     * The defaultHeaders are not applied when this signature of get is called.  (The authorization header
-     * if defined is applied.)  Specify and headers for the request in the headers method param.
-     */
-    K get(String url, InputStreamHandler inputStreamHandler, Collection<RequestHeader> headers = null) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.get()
-        addHeaders(builder, headers)
-        submitRequest(builder, url, inputStreamHandler)
+    // --- GET --- //
+    K get(String url, Collection<RequestHeader> headers = null) {
+        ClassicRequestBuilder builder = ClassicRequestBuilder.get(url)
+        return execute(builder, headers)
     }
 
-    K get(String url, String text) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.get()
-        setBody(builder, text)
-        addHeaders(builder, defaultHeaders)
-        submitRequest(builder, url)
+    K get(String url, InputStreamHandler streamHandler, Collection<RequestHeader> headers = null) {
+        ClassicRequestBuilder builder = ClassicRequestBuilder.get(url)
+        return execute(builder, headers, streamHandler)
     }
 
-    K get(String url, String text, InputStreamHandler inputStreamHandler, Collection<RequestHeader> headers = null) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.get()
-        setBody(builder, text)
-        addHeaders(builder, headers)
-        submitRequest(builder, url, inputStreamHandler)
+    // --- POST --- //
+    K post(String url, Collection<RequestHeader> headers = null) {
+        ClassicRequestBuilder builder = ClassicRequestBuilder.post(url)
+        return execute(builder, headers)
     }
 
-    K post(String url) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.post()
-        addHeaders(builder, defaultHeaders)
-        submitRequest(builder, url)
-    }
-
-    K post(String url, InputStreamHandler inputStreamHandler, Collection<RequestHeader> headers = null) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.post()
-        addHeaders(builder, headers)
-        submitRequest(builder, url, inputStreamHandler)
-    }
-
-    K post(String url, String text) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.post()
-        setBody(builder, text)
-        addHeaders(builder, defaultHeaders)
-        submitRequest(builder, url)
-    }
-
-    K post(String url, String text, InputStreamHandler inputStreamHandler, Collection<RequestHeader> headers = null) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.post()
-        setBody(builder, text)
-        addHeaders(builder, headers)
-        submitRequest(builder, url, inputStreamHandler)
+    K post(String url, String text, ContentType contentType = ContentType.APPLICATION_JSON, Collection<RequestHeader> headers = null) {
+        ClassicRequestBuilder builder = ClassicRequestBuilder.post(url)
+        builder.setEntity(new StringEntity(text, contentType))
+        return execute(builder, headers)
     }
 
     K post(String url, InputStream inputStream, Collection<RequestHeader> headers = null) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.post()
-        setInputStream(builder, inputStream)
-        addHeaders(builder, headers)
-        submitRequest(builder, url)
+        ClassicRequestBuilder builder = ClassicRequestBuilder.post(url)
+        builder.setEntity(new InputStreamEntity(inputStream, ContentType.APPLICATION_OCTET_STREAM))
+        return execute(builder, headers)
     }
 
-    K put(String url) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.put()
-        addHeaders(builder, defaultHeaders)
-        submitRequest(builder, url)
+    K post(String url, InputStreamHandler streamHandler, Collection<RequestHeader> headers = null) {
+        ClassicRequestBuilder builder = ClassicRequestBuilder.post(url)
+        return execute(builder, headers, streamHandler)
     }
 
-    K put(String url, InputStreamHandler inputStreamHandler, Collection<RequestHeader> headers = null) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.put()
-        addHeaders(builder, headers)
-        submitRequest(builder, url, inputStreamHandler)
+    // --- PUT --- //
+    K put(String url, Collection<RequestHeader> headers = null) {
+        ClassicRequestBuilder builder = ClassicRequestBuilder.put(url)
+        return execute(builder, headers)
+    }
+
+    K put(String url, String text, ContentType contentType = ContentType.APPLICATION_JSON, Collection<RequestHeader> headers = null) {
+        ClassicRequestBuilder builder = ClassicRequestBuilder.put(url)
+        builder.setEntity(new StringEntity(text, contentType))
+        return execute(builder, headers)
     }
 
     K put(String url, InputStream inputStream, Collection<RequestHeader> headers = null) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.put()
-        setInputStream(builder, inputStream)
-        addHeaders(builder, headers)
-        submitRequest(builder, url)
+        ClassicRequestBuilder builder = ClassicRequestBuilder.put(url)
+        builder.setEntity(new InputStreamEntity(inputStream, ContentType.APPLICATION_OCTET_STREAM))
+        return execute(builder, headers)
     }
 
-    K put(String url, String text) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.put()
-        setBody(builder, text)
-        addHeaders(builder, defaultHeaders)
-        submitRequest(builder, url)
+    // --- DELETE --- //
+    K delete(String url, Collection<RequestHeader> headers = null) {
+        ClassicRequestBuilder builder = ClassicRequestBuilder.delete(url)
+        return execute(builder, headers)
     }
 
-    K put(String url, String text, InputStreamHandler inputStreamHandler, Collection<RequestHeader> headers = null) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.put()
-        setBody(builder, text)
-        addHeaders(builder, headers)
-        submitRequest(builder, url, inputStreamHandler)
-    }
+    // --- Hooks for subclasses --- //
+    void beforeRequest(ClassicHttpRequest httpRequest) { }
 
-    K delete(String url) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.delete()
-        addHeaders(builder, defaultHeaders)
-        submitRequest(builder, url)
-    }
+    void afterRequest(ClassicHttpRequest httpRequest, K httpResponse) { }
 
-    K delete(String url, String text) {
-        ClassicRequestBuilder builder = ClassicRequestBuilder.delete()
-        setBody(builder, text)
-        addHeaders(builder, defaultHeaders)
-        submitRequest(builder, url)
-    }
+    // --- Private --- //
+    private K execute(ClassicRequestBuilder requestBuilder, Collection<RequestHeader> headers, InputStreamHandler streamHandler = null) {
+        // 1. Prepare URI
+        URI uri = requestBuilder.getUri()
+        if (uri == null) throw new InvalidUrlException("URL cannot be null")
+        if (uri.scheme == null) throw new InvalidUrlException(uri.toString(), "URL scheme cannot be null")
+        if (uri.host == null) throw new InvalidUrlException(uri.toString(), "URL host cannot be null")
 
-    void beforeRequest(ClassicHttpRequest httpRequest) {
+        // 2. Add all headers (default and per-request)
+        addHeaders(requestBuilder, headers)
 
-    }
-
-    void afterRequest(ClassicHttpRequest httpRequest, K httpResponse) {
-
-    }
-
-    void prepareRequest(ClassicRequestBuilder requestBuilder, String url) {
-        if (!url) throw new InvalidUrlException()
-        URI uri = new URI(url)
-        String scheme = uri.scheme
-        String host = uri.host
-        int port = uri.port ?: -1
-        if (!scheme) throw new InvalidUrlException(url, "null scheme for URL [${url}]")
-        if (!host) throw new InvalidUrlException(url, "null host for URL [${url}]")
-
-        HttpHost httpHost = new HttpHost(scheme, host, port) // -1 is default port for scheme
-        requestBuilder.setHttpHost(httpHost)
-        requestBuilder.setUri(uri)
-    }
-
-    K submitRequest(ClassicRequestBuilder requestBuilder, String url) {
-        prepareRequest(requestBuilder, url)
+        // 3. Build and execute
         ClassicHttpRequest request = requestBuilder.build()
-        K httpResponse = this.responseConstructor.newInstance()
+        K httpResponse = responseConstructor.newInstance()
+        httpResponse.requestUri = request.getRequestUri()
+        httpResponse.requestMethod = request.getMethod()
+
         beforeRequest(request)
-        httpResponse.textResponse = RequestProcessor.submitRequest(request)
+
+        internalClient.execute(request, {
+            response ->
+                httpResponse.statusCode = response.getCode()
+                httpResponse.reasonPhrase = response.getReasonPhrase()
+                httpResponse.locale = response.getLocale()
+
+                final HttpEntity entity = response.getEntity()
+                try {
+                    if (streamHandler != null) {
+                        entity.getContent().withStream(streamHandler.&call)
+                    } else {
+                        httpResponse.text = EntityUtils.toString(entity)
+                    }
+                } finally {
+                    EntityUtils.consume(entity)
+                }
+                return null
+        })
+
         afterRequest(request, httpResponse)
-        httpResponse
+        return httpResponse
     }
 
-    K submitRequest(ClassicRequestBuilder requestBuilder, String url, InputStreamHandler inputStreamHandler) {
-        prepareRequest(requestBuilder, url)
-        ClassicHttpRequest request = requestBuilder.build()
-        K httpResponse = this.responseConstructor.newInstance()
-        beforeRequest(request)
-        httpResponse.textResponse = RequestProcessor.submitRequest(request, inputStreamHandler)
-        afterRequest(request, httpResponse)
-        httpResponse
-    }
-
-    void setInputStream(ClassicRequestBuilder builder, InputStream inputStream) {
-        HttpEntity entity = EntityBuilder.create().setStream(inputStream).setContentType(ContentType.APPLICATION_OCTET_STREAM).build()
-        builder.setEntity(entity)
-    }
-
-    void setBody(ClassicRequestBuilder builder, String text) {
-        StringEntity entity = new StringEntity(text)
-        builder.setEntity(entity)
-    }
-
-    void addHeaders(ClassicRequestBuilder builder, Collection<RequestHeader> headers) {
+    private void addHeaders(ClassicRequestBuilder builder, Collection<RequestHeader> headers) {
+        // Apply defaults first
         if (authorizationHeader) {
             builder.setHeader(HttpHeaders.AUTHORIZATION, authorizationHeader)
         }
-        headers?.each { RequestHeader header ->
+        defaultHeaders?.each { RequestHeader header ->
             builder.addHeader(header.name, header.value)
         }
+        // Apply per-request headers, potentially overriding defaults
+        headers?.each { RequestHeader header ->
+            builder.setHeader(header.name, header.value) // setHeader overrides
+        }
     }
-
 }
