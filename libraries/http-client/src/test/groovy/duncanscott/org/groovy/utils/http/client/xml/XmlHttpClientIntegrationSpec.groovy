338d0f1000dd90cfeebe50f8eab6bc9a447d2789
Duncan Scott <dnscott@gmail.com>
2025-09-29 16:14:16 -0700

    http-client refector in progress

--- libraries/http-client/src/test/groovy/duncanscott/org/groovy/utils/http/client/xml/XmlHttpClientIntegrationSpec.groovy
+++ libraries/http-client/src/test/groovy/duncanscott/org/groovy/utils/http/client/xml/XmlHttpClientIntegrationSpec.groovy
@@ -1,7 +1,6 @@
-package duncanscott.org.groovy.http.xml
+package duncanscott.org.groovy.utils.http.client.xml
 
-import duncanscott.org.groovy.utils.http.client.xml.XmlHttpClient
-import duncanscott.org.groovy.utils.http.client.xml.XmlHttpResponse
+
 import spock.lang.Specification
 
 class XmlHttpClientIntegrationSpec extends Specification {
@@ -9,12 +8,6 @@ class XmlHttpClientIntegrationSpec extends Specification {
     void testGet() {
         setup:
         String url = 'https://gist.githubusercontent.com/franzwong/3838009/raw/e1c9a1fc2d047a5ab0fc38b9056166018614e79f/web.xml'
-        //String username = System.getenv('CLARITY_API_USER')
-        //String password = System.getenv('CLARITY_API_PASSWORD')
-
-        //expect:
-        //username
-        //password
 
         when:
         XmlHttpResponse xmlHttpResponse
@@ -33,7 +26,6 @@ class XmlHttpClientIntegrationSpec extends Specification {
         then:
         noExceptionThrown()
         node
-        //node.name() == 'prc:process'
         node.name().toString() == '{http://java.sun.com/xml/ns/javaee}web-app'
     }
 }
